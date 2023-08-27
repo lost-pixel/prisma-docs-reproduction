76be7a3f4bd1c7d1dbf25711ff8601ed7cb8527d
Chris Kalmar <christian.kalmar@gmail.com>
2023-08-27 14:49:38 +0200

    remove flaky retries

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -9,7 +9,6 @@ module.exports = {
   compareConcurrency: 10,
   shotConcurrency: 10,
   threshold: 10,
-  flakynessRetries: 2,
   beforeScreenshot: async (page) => {
     await page.addStyleTag({
       content: `iframe {
